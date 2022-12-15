01efcb34eb4d39c851e413752334d4a9c8f16fb5
mjezew <meganjezewski@gmail.com>
2022-12-14 16:49:24 -0800

    Add necessary packages to Brewfile; more thorough git config; more generic rally script repo filtering

--- shell/scripts/rally.sh
+++ shell/scripts/rally.sh
@@ -2,6 +2,6 @@
 set -eu
 RALLY=rally
 VERSION=0.0.1
-TARGET=$(ls -d  ~/dotfiles ~/desktop/* ~/desktop/gh/* ~/* | fzf)
+TARGET=$(ls -d  ~/gh/* ~/* | fzf)
 NAME=$(basename $TARGET)
 tmuxinator start $NAME || tmuxinator start default name=$NAME root=$TARGET
