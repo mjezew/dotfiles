f3a8a5227cab9875b340724f88a8133ba8203e50
mjezew <megan.jezewski@pdq.com>
2023-05-24 12:20:32 -0700

    more db setup

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -188,4 +188,17 @@ return require("packer").startup(function(use)
       )
     end
   }
+  use {
+    'tpope/vim-dadbod',
+    requires = {
+      'kristijanhusak/vim-dadbod-ui',
+      'kristijanhusak/vim-dadbod-completion'
+    },
+    config = function()
+      vim.g.db_ui_use_nerd_fonts = true
+      vim.g.db_ui_execute_on_save = false
+      vim.g.db_ui_save_location = "./mjezew_queries"
+      vim.g.db_ui_disable_mappings = true
+    end
+  }
 end)
